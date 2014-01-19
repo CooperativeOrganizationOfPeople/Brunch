e27324d44534c8a2207298db45731ff637666448
nicholasffox <nicholas.f.fox@gmail.com>
2014-01-18 21:48:34 -0500

    fox: currently commented out extra fields in the model

--- brunch_buddy/brunch_app/models.py
+++ brunch_buddy/brunch_app/models.py
@@ -9,11 +9,11 @@ class Restaurant(models.Model):
     #location = models.ForeignKey(Neighborhood)
     location = models.CharField(max_length=50)	# defaults to a Neighborhood name, falls back on the City name
     status = models.BooleanField()	# is it 'to-do' or 'done'?
-    phone = models.CharField(max_length=20)
-    display_phone = models.CharField(max_length=20)
-    categories = models.CharField(max_length=200)
-    rating = models.IntegerField()
-    review_count = models.IntegerField()
+    #phone = models.CharField(max_length=20)
+    #display_phone = models.CharField(max_length=20)
+    #categories = models.CharField(max_length=200)
+    #rating = models.IntegerField()
+    #review_count = models.IntegerField()
     #rating_img_url = models.URLField()
     #yelp_url = models.URLField()
     #yelp_mobile_url = models.URLField()
